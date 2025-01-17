5851e1ddba6b581057235501ac850a3c34ee010b
Aimee Hong <honglp@bc.edu>
2025-01-17 11:58:03 -0500

    created simple nav bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import "./../styles/Navbar.css";
+
+export default function NavigationBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Quizzes</a>
+        </li>
+        <li>
+          <a href="#">Profile</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
